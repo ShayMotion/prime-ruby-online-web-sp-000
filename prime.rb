c7c8f1c953cdd593f9101d239f1120357ee62f14
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-08-21 15:11:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,12 @@
-# Add  code here!
-array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14]
-
-prime?(i)
+def prime?(number)
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |num_to_test| #none of the numbers should return true for none? to return true. If none of the numbers
+                                # return true, then they are not divisible by the number and the number is prime
+      number % num_to_test == 0
+    end
+  else
+    false
+  end
+end
